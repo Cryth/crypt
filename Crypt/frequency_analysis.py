23980bb8c1ac39093c916041fe04723ad4c9697f
romanstrazanec <32615292+romanstrazanec@users.noreply.github.com>
2018-03-16 11:31:47 +0100

    Update frequency_analysis.py
    can make frequency analysis up to four files

--- Crypt/frequency_analysis.py
+++ Crypt/frequency_analysis.py
@@ -1,38 +1,66 @@
+# coding=utf-8
+import numpy as np
 import matplotlib.pyplot as plt
 
 
 chars = list("abcdefghijklmnopqrstuvwxyz")
 ci = [num for num in range(len(chars))]
-ca = [0 for _ in range(len(chars))] # char amounts
-col = 'red'
-cp = 0 # celkovy pocet
+col = ['r', 'b', 'g', 'c']
+
+files = [] # file names
+file_ca = [] # lists of character amounts
 
 def main():
-    file = input("File path: ")
-    _freqfile(file)
-    _plot(file)
+    print("\n\tFREKVENČNÁ ANALÝZA SÚBOROV\n")
+
+    for _ in col:
+        file = input("File path: ")
+        if file == "" or file == " ":
+            break
+        files.append(file)
+        _freqfile(file)
+    if len(files) == 1:
+        _bar(files[0])
+    else:
+        _plot()
 
 
 def _freqfile(file_name):
     with open(file_name, "r") as f:
+        cp = 0  # celkovy pocet
+        ca = [0 for _ in range(len(chars))]  # char amounts
+
         while True:
-            ch = f.read(1)
+            ch = f.read(1).lower()
             if not ch:
                 break
             if ch in chars:
                 ca[chars.index(ch)] += 1
-                global cp
                 cp += 1
+        np_ca = np.array(ca)
+        np_ca = np_ca / cp * 100 # percentá
+        file_ca.append(np_ca)
 
 
-def _plot(file_name):
-    plt.plot(ci, ca, color=col)
+def _bar(file_name):
+    plt.bar(ci, file_ca[0], color=col[0])
     plt.title("Frekvenčná analýza súboru {}".format(file_name))
-    plt.ylabel("Počet")
+    plt.ylabel("Percentá")
     plt.xticks(ci, chars)
-    for i, val in enumerate(ca):
-        per = round(val / cp * 100, 2) # percento
-        plt.text(i,val,str(per)+"%")
+    for i, val in enumerate(file_ca[0]):
+        per = round(val, 2)
+        plt.text(i-.45, val+.2, str(per) + "%")
+    plt.grid(True)
+    plt.show()
+
+
+def _plot():
+    for i, ca in enumerate(file_ca):
+        plt.plot(ci, ca, color=col[i], label=files[i])
+    plt.title("Frekvenčná analýza súborov {}".format(files))
+    plt.legend(title="Súbory", loc='right', bbox_to_anchor=(0.5, 1.05), fancybox=True, shadow=True)
+    plt.ylabel("Percentá")
+    plt.xticks(ci, chars)
     plt.grid(True)
     plt.show()
 
